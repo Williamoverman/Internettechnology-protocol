be4113496dde250481bd8038fdb650c5359f3394
William Overman <568354@student.saxion.nl>
2025-12-02 17:56:43 +0100

    small changes to socketmanager

--- server_java/src/SocketManager.java
+++ server_java/src/SocketManager.java
@@ -1,25 +1,38 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class SocketManager {
     private ServerSocket serverSocket;
-    private boolean running;
+    private volatile boolean running;
+    private ExecutorService executor;
 
     public void setup() throws IOException {
         serverSocket = new ServerSocket(Config.SERVER_PORT);
         running = true;
+        executor = Executors.newFixedThreadPool(40);
 
         while (running) {
-            Socket clientSocket = serverSocket.accept();
-            new Thread(new ClientHandler(clientSocket)).start();
+            try {
+                Socket clientSocket = serverSocket.accept();
+                executor.submit(new ClientHandler(clientSocket));
+            } catch (IOException e) {
+                if (running)
+                    System.err.println("Error accepting client connection " + e.getMessage());
+            }
         }
     }
 
     public void stop() {
         running = false;
         try {
-            serverSocket.close();
+            if (serverSocket != null && !serverSocket.isClosed())
+                serverSocket.close();
+
+            if (executor != null)
+                executor.shutdownNow();
         } catch (IOException e) {
             throw new RuntimeException(e.getMessage());
         }
