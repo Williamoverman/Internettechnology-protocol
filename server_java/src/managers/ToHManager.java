1b2705ccaaace6a6e3b09306f2e9d95b0a5bfacc
WilliamOverman <568354@student.saxion.nl>
2025-12-15 00:21:41 +0100

    redo toh game

--- server_java/src/managers/ToHManager.java
+++ server_java/src/managers/ToHManager.java
@@ -1,11 +1,5 @@
 package managers;
 
 public class ToHManager {
-    private static final ToHManager instance = new ToHManager();
 
-    private ToHManager() {}
-
-    public static ToHManager getInstance() {
-        return instance;
-    }
 }
